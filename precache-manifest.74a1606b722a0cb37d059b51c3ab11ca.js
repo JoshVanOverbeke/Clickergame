482bcba44ed10582390b76b96366e3f51f98f129
Josh VanOverbeke <joshvanoverbeke@gmail.com>
2019-03-25 21:43:13 -0500

    Updates

---
+++ precache-manifest.74a1606b722a0cb37d059b51c3ab11ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f7f8886fbb3982c54ed",
+    "url": "/Clickergame/static/js/runtime~main.ac64d874.js"
+  },
+  {
+    "revision": "b795f7c7229e53dce409",
+    "url": "/Clickergame/static/js/main.31e13d7f.chunk.js"
+  },
+  {
+    "revision": "88048034b74e1a7b32d9",
+    "url": "/Clickergame/static/js/2.599baac1.chunk.js"
+  },
+  {
+    "revision": "b795f7c7229e53dce409",
+    "url": "/Clickergame/static/css/main.fab3e142.chunk.css"
+  },
+  {
+    "revision": "393621e42584283011d37dea495c97dc",
+    "url": "/Clickergame/index.html"
+  }
+];
